58795d1e9bd3d9da7daa04e68ef8811bd81bfbe5
Popovyc-h <popovichtanja86@gmail.com>
2026-05-17 17:01:31 +0300

    Update Program.cs

--- task-6/Program.cs
+++ task-6/Program.cs
@@ -1,1 +1,38 @@
+using System.Net.NetworkInformation;
 
+namespace NetworkScanner;
+
+internal class Program
+{
+    public static bool PingHost(string ipInput)
+    {
+        var ping = new Ping();
+        PingReply reply = ping.Send(ipInput);
+        
+        if (reply.Status == IPStatus.Success)
+            return true;
+        
+        return false;
+    }
+
+    public static PingReply GetPingTime(string ipInput)
+    {
+        var ping = new Ping();
+        PingReply reply = ping.Send(ipInput);
+
+        return reply;
+    }
+
+    static void Main(string[] args)
+    {
+        Parallel.For(1, 255, i =>
+        {
+            string ip = $"192.168.0.{i}";
+            PingReply reply = GetPingTime(ip);
+            if (PingHost(ip))
+            {
+                Console.WriteLine($"{ip} - активний | {reply.RoundtripTime}ms");
+            }
+        });
+    }
+}
